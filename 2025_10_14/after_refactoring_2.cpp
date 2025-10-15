5e30e258b6e45ffb881bba9a9ecf144c0ee476c5
Dersw <htno2007@gmail.com>
2025-10-15 14:30:40 +0300

    do refacroring

---
+++ 2025_10_14/after_refactoring_2.cpp
@@ -0,0 +1,34 @@
+﻿#include <cmath>
+#include <iostream>
+
+int main()
+{   
+    const int arr_size = 10;
+    double* arr = new double[arr_size];
+
+    for (int i = 0; i < 10; i++) {
+        std::cout << "Введите " << i + 1 << " элемент: " << std::endl;
+        std::cin >> arr[i];
+    }
+
+    int i = 0;
+    bool is_increasing = true;
+    while (i < arr_size - 1) {
+      if (arr[i + 1] < arr[i]) {
+        is_increasing = false;
+        break;
+      }
+      i++;
+    }
+    
+    delete[] arr;
+
+    if (is_increasing) {
+        std::cout << "Последовательность возврастающая" << std::endl;
+    }
+    else {
+        std::cout << "Последовательность не возврастающая" << std::endl;
+    }
+
+    return 0;
+}
