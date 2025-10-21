0ced6d775aafea44d55b080e24f937e5b03e8aac
Dersw <htno2007@gmail.com>
2025-10-21 14:08:03 +0300

    fix standart library include

--- task2_module_app/main.cpp
+++ task2_module_app/main.cpp
@@ -1,8 +1,9 @@
+#include <iostream>
+
 #include "generator.hpp"
 #include "io.hpp"
 #include "prefix_sum.hpp"
 #include "request_handler.hpp"
-#include "iostream"
 
 int main() {
     int arr_len;
@@ -11,14 +12,12 @@ int main() {
     std::cin >> arr_len;
     int* arr = drw::generate_arr(arr_len);
     drw::print_array("Cгенерированный массив: ", arr, arr_len);
-    int* prefix_sum_arr = drw::calc_prefix_sum(arr, arr_len, max_val);
-    delete[] arr;
     
     int requests_amount;
     std::cout << std::endl << "Введите желаемое количество запросов: " << std::endl;
     std::cin >> requests_amount;
-    drw::handle_requests(prefix_sum_arr, requests_amount, max_val);
-    delete[] prefix_sum_arr;
+    drw::handle_requests(arr, arr_len, requests_amount, max_val);
+    delete[] arr;
 
     return 0;
 }
